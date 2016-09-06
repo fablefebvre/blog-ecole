11cd55ca54a6344bc54b40fa7c42f4d34f3ad7ac
Fabrice LEFEBVRE <fabricelefebvre@MacBook-de-fabrice-LEFEBVRE.local>
2016-09-06 22:46:08 +0200

    Creating Avatar component

--- app/index.js
+++ app/index.js
@@ -1,12 +1,53 @@
+var USER_DATA = {
+    name: 'Fabrice LEFEBVRE',
+    username: 'fab.lefebvre',
+    image: 'https://avatars1.githubusercontent.com/u/725674'
+}
+
 var React = require('react')
 var ReactDOM = require('react-dom')
-var HelloWorld = React.createClass({
-  render: function(){
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
+
+var ProfilePic = React.createClass({
+   render: function(){
+       return (
+           <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+       )
+   }
 });
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
+var ProfileLink = React.createClass({
+   render: function(){
+       return (
+           <div>
+            <a href={'https://github.com/' + this.props.username}>
+                {this.props.username}
+            </a>
+           </div>
+       )
+   } 
+});
+
+var ProfileName = React.createClass({
+    render: function(){
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Avatar user={USER_DATA} />,
+    document.getElementById('app')
+);
